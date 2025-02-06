6a494f5a915b28ab82f365e5f9badc5a600fcdfb
PeelayMurtuza <peelaymurtuza@gmail.com>
2025-02-06 23:25:33 +0530

    Defines permissions for admin and user roles.

---
+++ backend/src/authorization/permission-keys.ts
@@ -0,0 +1,12 @@
+export const enum PermissionKeys {
+  // Admin permissions
+  CreateJob = 'CreateJob',
+  UpdateJob = 'UpdateJob',
+  DeleteJob = 'DeleteJob',
+
+  // User permissions
+  AccessAuthFeature = 'AccessAuthFeature',
+  ViewProfile = 'ViewProfile',
+  EditProfile = 'EditProfile',
+  // Add more permissions as needed
+}
